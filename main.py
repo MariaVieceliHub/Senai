fb1acd327d6e4e1c8e061b4e45dc79bfba64894f
MariaVieceliHub <maria_vieceli@estudante.sesisenai.org.br>
2025-05-14 14:20:44 +0000

    update

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import os
 template_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__, template_folder=template_dir)
 
+# Direcionamento para o index HTML
+@app.route("/")
+def home ():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host= "0.0.0.0", port=3000)
+    
